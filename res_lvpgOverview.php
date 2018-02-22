fa113641adcddd6406574b7e59e0f193aacf5cf2
bt <25850024+bfranzen19@users.noreply.github.com>
2018-02-22 11:10:10 -0700

    removed script tags

--- res_lvpgOverview.php
+++ res_lvpgOverview.php
@@ -15,37 +15,11 @@
   // $data = str_replace('" == $0', "", $data);
 
 // break scripts
-  $data = strip_tags($data, "<a><strong><script><img><b><p><br><hr><h1><h2><h3><div><ul><li><section><article>");
+  $data = strip_tags($data, "<a><strong><img><b><p><br><hr><h1><h2><h3><div><ul><li><section><article>");
     // strips all tags EXCEPT for tags listed to the right (second input). if it's removed from
     // the second input, it's going to be stripped.
 
   echo "loadData(".json_encode($data).")";
-
-// z php
-?>
-
-
-<?php
-// URL to scrape
-  // $url = "https://www.lvhn.org/about_us";
-
-// scraper
-  // $curl = curl_init($url);
-  // curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-  // curl_setopt($curl, CURLOPT_HEADER, 0);
-  // $data = curl_exec($curl);
-//
-//   curl_close($curl);
-//
-//   $data = str_replace('"string(64553) "', "", $data);
-//   $data = str_replace('" == $0', "", $data);
-//
-// // break scripts
-//   $data = strip_tags($data, "<a><strong><script><img><b><p><br><hr><h1><h2><h3><div><ul><li><section><article>");
-//     // strips all tags EXCEPT for tags listed to the right (second input). if it's removed from
-//     // the second input, it's going to be stripped.
-//
-//   echo "loadData(".json_encode($data).")";
 
 // z php
 ?>
